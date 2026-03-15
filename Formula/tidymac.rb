bad7828abc1d082ac216a4d8549b4e4aa12167ff
Mazin Salim <mazinksa@live.com>
2026-03-15 14:51:29 -0500

    chore: bump to v0.2.0 with release SHA256 hashes

--- Formula/tidymac.rb
+++ Formula/tidymac.rb
@@ -1,16 +1,16 @@
 class Tidymac < Formula
   desc "A lightweight TUI system cleaner for macOS"
   homepage "https://github.com/therealmazin/tidymac"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/therealmazin/tidymac/releases/download/v#{version}/tidymac-v#{version}-darwin-arm64.tar.gz"
-      sha256 "48a0ccc0590edae81f4c81ff6e6b8c7f496368c2c655ffbbedb42e8f0b252ed8"
+      sha256 "51a3c95ca1ca0e30e369e430eef3101dd62e8e0b0dd2ccaf8d04f5be5cd0a0c6"
     elsif Hardware::CPU.intel?
       url "https://github.com/therealmazin/tidymac/releases/download/v#{version}/tidymac-v#{version}-darwin-x64.tar.gz"
-      sha256 "3c02c32fb72eb388c1729d9cc4c808cfd9876d27245ac8d98fcbe81b3c73bae2"
+      sha256 "00fa6cf0c1a323836b9a2aedbf449ac620dbd61793e7ade808a9e7b31e32d7b0"
     end
   end
 
